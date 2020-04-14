08dabfbd481866a6825ff6a9da1fa6e320d25e73
Sage <47766817+skonstanzer@users.noreply.github.com>
2020-04-14 16:25:30 -0500

    sage code

---
+++ sage code/src/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import './App.css';
+import { SettingsForm } from './settingsForm.jsx';
+import { ItemDetails } from './itemDetails.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <ItemDetails />
+      </>
+    );
+  }
+}
+
+export default App;
