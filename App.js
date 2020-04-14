bf0cd0d56360136ddc304e1a37f33ca0963f0f73
ABrkich <47767559+ABrkich@users.noreply.github.com>
2020-04-14 14:19:00 -0700

    Login Page + inventory page
    need 2 link API but general navigation features work, super easy to add more pages to this

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Login} from './login';
+import {Inventory} from './inventory';
+import {Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Router>
+        <Switch>
+         <Route exact path="/" component={Login} />
+        <Route path="/inventory" component={ Inventory} />
+          <Route path="/login" component={ Login} />
+        </Switch>
+        </Router>
+      </header>
+    </div>
+  );
+  //  <Link to="/">Login</Link>{' '}
+  //  <Login />
+  // <Router>
+  //   <Switch>
+  //   <Route exact path="/" component={ Login } />
+  //   </Switch>
+  // </Router>
+  // <Route exact path="/" component={ AccountsDashboard } />
+  // <Route path="/create" component={ AccountEditor } />
+  // <Route path="/edit/:accountId" component={ AccountEditor } />
+}
+
+export default App;
